ea1f0792bb484ba5dfcb5f50498324f5f4a0853c
Oneted11 <muriithited@gmail.com>
2022-06-26 06:37:26 +0300

    feat:added navbar little styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png";
+
+function Navbar() {
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", height: "48px", margin: "0,20px" }}
+        ></img>
+      </Link>
+      <Stack>
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #ff2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3A1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const Root = ReactDOM.createRoot(document.getElementById("root"));
 
-Root.render(<App />);
+Root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
