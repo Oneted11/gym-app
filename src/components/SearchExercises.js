9c1d716b53a2d2850726fc50a35261e090c81f73
Oneted11 <muriithited@gmail.com>
2022-06-28 12:25:43 +0300

    feat: added searchbar with styling

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Button, TextField, Typography } from "@mui/material";
 
 function SearchExercises() {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState("");
+
+  const handlesearch = async () => {
+    if (search) {
+      // const exerciseData=await fetchdata()
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" p="20px" justifyContent="center">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises you <br />
+        Should know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderradius: "40px",
+          }}
+          height="76px"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
+          placeholder="Search exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#ff2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handlesearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 }
 
 export default SearchExercises;
